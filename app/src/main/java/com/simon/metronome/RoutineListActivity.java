bf999879dc7a988e73eea9c979205b833e310a34
Simon Zweers <s.zweers21@gmail.om>
2023-09-30 23:13:06 +0200

    Started with Recyclerview

---
+++ app/src/main/java/com/simon/metronome/RoutineListActivity.java
@@ -0,0 +1,15 @@
+package com.simon.metronome;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class RoutineListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routine_list);
+    }
+}
