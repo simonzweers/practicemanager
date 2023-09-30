6e48347179b69dce6be4c0ab981f7956d80b11a0
Simon Zweers <s.zweers21@gmail.om>
2023-09-30 22:47:24 +0200

    Changed app idea to practice routine scheduler

--- app/src/main/java/com/simon/metronome/MainActivity.java
+++ app/src/main/java/com/simon/metronome/MainActivity.java
@@ -3,53 +3,33 @@ package com.simon.metronome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    MetronomeContext metronome;
-    Button startStopButton;
+    public static String BUTTON_PRESS_TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView metronomeMark = findViewById(R.id.textView);
-        SeekBar speedSlider = findViewById(R.id.seekBar);
-        metronome = new MetronomeContext(getApplicationContext());
-        metronome.startMetronome();
-        startStopButton = findViewById(R.id.start_stop_button);
-        speedSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                int value = speedSlider.getProgress();
-                metronomeMark.setText("Value of slider: " + value);
-                metronome.setBpm(value);
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+        Button addButton = (Button) findViewById(R.id.button_add);
+        Button removeButton = (Button) findViewById(R.id.button_remove);
+        addButton.setOnClickListener(view -> {
+            Log.i(BUTTON_PRESS_TAG, "Add Button Pressed");
         });
-        startStopButton.setOnClickListener(view -> {
-            if (metronome.getMetronomeOn()) {
-                metronome.stopMetronome();
-            } else {
-                metronome.startMetronome();
-            }
+        removeButton.setOnClickListener(view -> {
+            Log.i(BUTTON_PRESS_TAG, "Remove button pressed");
         });
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        metronome.stopMetronome();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        metronome.startMetronome();
     }
 }
